247e5525e260c0ee9d845767b728d5fbb9932d74
Rafael Silveira Rocha <rafarocha1511@gmail.com>
2023-08-16 09:48:20 -0300

    feat: added StatusNotAllowedException

---
+++ src/main/java/rafael/rocha/compasschallenge/exceptions/StatusNotAllowedException.java
@@ -0,0 +1,7 @@
+package rafael.rocha.compasschallenge.exceptions;
+
+public class StatusNotAllowedException extends RuntimeException{
+    public StatusNotAllowedException(String message) {
+        super(message);
+    }
+}
